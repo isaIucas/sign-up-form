f97555755987433ed70a571a449ca7818ab120a2
thatsMind <collinzizhen@gmail.com>
2022-04-05 14:51:11 -0700

    release version 1.0 of sign up form

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmpass");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    password.setCustomValidity("Passwords Don't Match");
+
+  } else {
+    confirm_password.setCustomValidity('');
+    password.setCustomValidity('');
+
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
